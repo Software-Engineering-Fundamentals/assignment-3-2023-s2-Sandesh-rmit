f3122fbf26348d69b928835edbeeffd9782a9a6f
nicholassitoRMIT <s3951974@student.rmit.edu.au>
2023-10-01 22:48:59 +1100

    scenarios explained

--- InstructorRegistration/main.java
+++ InstructorRegistration/main.java
@@ -2,6 +2,7 @@ package InstructorRegistration;
 
 public class Main{
     public static void main(String args[]){
-
+        // this file will be responsible for code related to the following scenario:
+        // Instructor registers a course on the learning portal/website
     }
 }
--- LodgeInquiry/Main.java
+++ LodgeInquiry/Main.java
@@ -2,6 +2,7 @@ package LodgeInquiry;
 
 public class Main {
     public static void main(String args[]) {
-
+        // this file will be responsible for code related to the following scenario:
+        // Lodge a general inquiry about the portal which is successfully addressed by the Manager
     }
 }
