da37e811203c3c29a05f6a65f9e58b249db1f85e
nicholassitoRMIT <s3951974@student.rmit.edu.au>
2023-10-01 22:28:05 +1100

    added getter and setter methods into instructor

--- InstructorRegistration/Instructor.java
+++ InstructorRegistration/Instructor.java
@@ -15,4 +15,28 @@ public class Instructor {
         this.name = name;
         this.username = username;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
