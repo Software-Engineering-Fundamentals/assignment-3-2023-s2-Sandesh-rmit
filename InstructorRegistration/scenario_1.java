991c7206347885a22dc36c9aa149637e1c3ad670
Sandesh Mangat <s3945006@student.rmit.edu.au>
2023-10-02 23:54:49 +1100

    g

--- InstructorRegistration/scenario_1.java
+++ InstructorRegistration/scenario_1.java
@@ -5,10 +5,13 @@ public class scenario_1{
         // this file will be responsible for code related to the following scenario:
         // Instructor registers a course on the learning portal/website
         Instructor teacher = new Instructor("Daniel@gmail", "Daniel", "Daniel_1");
+        Courses enrolment = new Courses("Maths", "Daniel", "Fun class", false)
 
         String name = teacher.getName();
+        String class = enrolment.title();
 
         System.out.println(name);
+        System.out.println(class);
         
     }
 }
