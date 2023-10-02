5ee06195729f8461405cdb0daf15ff6695c83862
Vidhathra <s3944867@student.rmit.edu.au>
2023-10-02 21:06:00 +1100

    stidnt class edit and get added

--- InstructorRegistration/Student.java
+++ InstructorRegistration/Student.java
@@ -1,5 +1,36 @@
 package InstructorRegistration;
 
-public class Student {
+import java.util.*;
 
+public class Student {
+    String email;
+    String name;
+    String username;
+    List<String> course = new ArrayList<>();
+
+    public Student() {
+        email = null;
+        name = null;
+        username = null;
+    }
+
+    public Student(String email, String name, String username) {
+        this.email = email;
+        this.name = name;
+        this.username = username;
+    }
+
+    public void editCourses() {
+        String courseName = "duifue";
+        this.course.add(courseName);
+    }
+
+    // returns a list of Courses
+    public List<String> getCourses() {
+        return this.course;
+    }
+
+    // public void quitCourse() {
+
+    // }
 }
