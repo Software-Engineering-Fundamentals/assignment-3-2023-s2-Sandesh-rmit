ae4918eee4d0d193d106d3ad5ebac00cb789d22f
Sandesh Mangat <s3945006@student.rmit.edu.au>
2023-10-02 23:33:08 +1100

    main instructor

--- InstructorRegistration/main.java
+++ InstructorRegistration/main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String args[]){
         // this file will be responsible for code related to the following scenario:
         // Instructor registers a course on the learning portal/website
-
+        Instructor teacher = new Instructor("Daniel@gmail", "Daniel", "Daniel_1");
         
     }
 }
