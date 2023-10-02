f896fde74656d7542e39de0a7515144c3e8c84e1
Vidhathra <s3944867@student.rmit.edu.au>
2023-10-02 23:43:54 +1100

    main edits

--- LodgeInquiry/Main.java
+++ LodgeInquiry/Main.java
@@ -3,6 +3,8 @@ package LodgeInquiry;
 public class Main {
     public static void main(String args[]) {
         // this file will be responsible for code related to the following scenario:
-        // Lodge a general inquiry about the portal which is successfully addressed by the Manager
+        // Lodge a general inquiry about the portal which is successfully addressed by
+        // the Manager
+
     }
 }
