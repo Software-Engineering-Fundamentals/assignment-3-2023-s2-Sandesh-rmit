a30c83315af8a2f9f7d4c92d41927bb3186ee2e9
Vidhathra <s3944867@student.rmit.edu.au>
2023-10-02 23:16:57 +1100

    student commment and constructor added

--- LodgeInquiry/Student.java
+++ LodgeInquiry/Student.java
@@ -1,67 +1,77 @@
 package LodgeInquiry;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     String email;
-    String course;
+    List<String> course = new ArrayList<>();
     String password;
-    String userName;
+    String username;
     boolean quitCourse;
     boolean refundApproved;
     String question;
     String responses;
 
-    public String getCourses() {
+    // constructor which gives password, username a default value
+    public Student() {
+        this.email = null;
+        this.username = null;
+        this.password = null;
+    }
+
+    public Student(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    // returns the course that student is enroled in
+    public List<String> getCourses() {
         return course;
     }
 
+    // adds courseName into ArrayList course
+    public void editCourses(String courseName) {
+        course.add(courseName);
+    }
+
+    // returns responses from the instructor to the student's questions
     public String receiveResponse() {
         return responses;
     }
 
+    // adds question to string with \n to seperate them
     public void addQuestion(String sentence) {
         question += sentence + "\n";
     }
 
+    // returns the student's question
     public String returnQuestion() {
         return question;
     }
 
-    public void changeEmail(String newEmail) {
-        email = newEmail;
-        // store new email address into address variable
+    // store new email address into address variable
+    public void changeEmail(String email) {
+        this.email = email;
     }
 
+    // returns the stored email address of student
     public String getEmail() {
         return email;
     }
 
-    public void changePassword(String newPassword) {
-        password = newPassword;
-        // store new password into password variable
+    // store new password into password variable
+    public void changePassword(String password) {
+        this.password = password;
     }
 
-    public String getPassword() {
-        return password;
+    // remove courseName from Arraylist course
+    public void quitCourse(String courseName) {
+        this.course.remove(courseName);
     }
 
-    public void changeUserName(String newUserName) {
-        userName = newUserName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public boolean quitCourse(boolean quit) {
-        quitCourse = false;
-
-        if (quit == true) {
-            quitCourse = quit;
-        }
-
-        return quitCourse;
-    }
-
+    // returns True if refund is applicable
     public boolean applyForRefund(boolean refund) {
         refundApproved = false;
 
