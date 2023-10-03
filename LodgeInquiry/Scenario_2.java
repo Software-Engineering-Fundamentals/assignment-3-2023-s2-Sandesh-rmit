e8329b4aae8131c3e40a65bc2287b4ae6aaa5a30
Sandesh Mangat <s3945006@student.rmit.edu.au>
2023-10-03 16:45:19 +1100

    comment

--- LodgeInquiry/Scenario_2.java
+++ LodgeInquiry/Scenario_2.java
@@ -16,10 +16,17 @@ public class Scenario_2 {
         Manager manager = new Manager(mEmail, mName);
 
         String a = student.sendEnquiry("Can i have a discount?");
-        String b = manager
-                .respondToEnquiry("No, unfortunately we don't have promotions to able to provide any discounts");
+        // calling the sendEnquiry method from Student class and storing the message
+        String b = manager.respondToEnquiry("No, unfortunately we don't provide any discounts"); 
+        // calling the respondToEnquiry method from Manager class which responds to the enquiry sent by a student
 
         System.out.println(a);
+        // printing out the message from student enquiry
         System.out.println(b);
+        // printing out the response from the manager
+
+        /*  The 2 System.out.print() are used to check if the enquiries are going to the terminal and have 
+          no issues with the system */
     }
+
 }
